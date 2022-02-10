a07f52cec5bc14b50396a4b1f8c1fcc523613f96
aaaka5h <akash7adhikary@gmail.com>
2022-02-10 15:08:27 -0800

    Working countdown

---
+++ main.js
@@ -0,0 +1,25 @@
+const countdown = () => {
+    const countDate = new Date("May 1, 2022 00:00:00");
+    const currDate = new Date().getTime();
+    const timeRemaining = countDate - currDate;
+
+    // time vars
+    const ms = 1
+    const second = 1000 * ms;
+    const minute = 60 * second;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+
+    const dayText = Math.floor(timeRemaining / day);
+    const hourText = Math.floor((timeRemaining % day) / hour);
+    const minuteText = Math.floor((timeRemaining % hour) / minute);
+    const secondText = Math.floor((timeRemaining % minute) / second);
+
+    // display
+    document.querySelector(".day").innerText = dayText;
+    document.querySelector(".hour").innerText = hourText;
+    document.querySelector(".minute").innerText = minuteText;
+    document.querySelector(".second").innerText = secondText;
+};
+
+setInterval(countdown, 1000);
